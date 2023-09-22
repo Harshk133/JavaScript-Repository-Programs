47c4ebf4ffd11ceef2c95f4769454d6651a4b964
Harsh <harshmkale.2004@gmail.com>
2023-09-22 21:40:43 +0530

    Program11.js

---
+++ js/Program11.js
@@ -0,0 +1,24 @@
+console.group("Window Object in JavaScript!");
+
+// let a = window;
+let a = window.document;
+// window.alert("hello, javascript!");
+// alert("hello, javascript!");
+// a = prompt("I am a hacker!😈");
+// a = confirm("Are You sure to delete following work??!");
+
+a = window.innerHeight; // innerHeight both are same
+a = window.innerWidth; // innerWidth both are same
+a = window.scrollX; // scrollX both are same
+a = window.scrollY; // scrollY both are same
+a = location; // window.location both are same
+a = location.toString();
+a = window.history;
+
+
+console.log(a);
+
+
+
+
+console.groupEnd();
